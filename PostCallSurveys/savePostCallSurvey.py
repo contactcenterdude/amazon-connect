6cced6f31d033926fb9c6343cf6ff02e111b65a5
maximtsvetov <75195366+maximtsvetov@users.noreply.github.com>
2021-12-05 19:06:19 -0500

    Add files via upload

---
+++ PostCallSurveys/savePostCallSurvey.py
@@ -0,0 +1,38 @@
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime
+
+def lambda_handler(event, context):
+
+
+    print(event)
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('PostCallSurveys')
+    
+    ContactId=event['Details']['ContactData']['ContactId']
+    CLID=event['Details']['ContactData']['CustomerEndpoint']['Address']
+    
+    current_date =datetime.now()
+    curren_date_string=current_date.strftime("%Y-%m-%d %H:%M:%S")
+    
+    #answer="1"
+    
+    answer=event['Details']['ContactData']['Attributes']['answer']
+    
+    try:
+        response = table.put_item(
+           Item={
+                'ContactId':ContactId,
+                'phoneNumber': CLID,
+                'datetime': curren_date_string,
+                'answer': answer
+            }
+        )
+        
+        return response
+
+    
+            
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    
